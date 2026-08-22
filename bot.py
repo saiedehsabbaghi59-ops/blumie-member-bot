52fd79a1e8df3b1bd0d637421433f8506b719f56
saiedehsabbaghi59-ops <saiedeh.sabbaghi59@gmail.com>
2026-08-22 22:17:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import os
-
 from bale import Bot, Message
 
-# دریافت اطلاعات محرمانه از تنظیمات Render
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
@@ -13,17 +11,20 @@ bot = Bot(token=BOT_TOKEN)
 
 @bot.event
 async def on_ready():
-    print(f"🌸 {bot.user.username} is ready!")
+    print("🌸 Blumie Bot is ready!")
 
 
 @bot.event
 async def on_message(message: Message):
-    if message.content == "/start":
+    if not message.content:
+        return
+
+    if message.content.strip() == "/start":
         await message.reply(
             "🌸 به ممبرگیر بلومی خوش اومدی!\n\n"
             "🪙 اینجا می‌تونی سکه جمع کنی، "
             "برای کانالت سفارش ثبت کنی و از امکانات بلومی استفاده کنی.\n\n"
-            "✨ از منوی زیر شروع کن:"
+            "✨ برای شروع /start رو بزن."
         )
 
 
